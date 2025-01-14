29c459c81fb56d008730a09b68de83c81efda940
BRICE JOSHY <bricejoshy99@gmail.com>
2025-01-14 16:34:58 +0530

    nav bar assets and icons

--- lib/screen_1.dart
+++ lib/screen_1.dart
@@ -1,3 +1,4 @@
+import 'package:app/bottomNav.dart';
 import 'package:flutter/material.dart';
 
 class Screen1 extends StatelessWidget {
@@ -5,8 +6,8 @@ class Screen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Get the height of the screen
     final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: const Color(0xFF0F1115),
@@ -15,15 +16,87 @@ class Screen1 extends StatelessWidget {
         top: false,
         child: Stack(
           children: [
-            // Positioned background image
+            // Background Image
             Positioned(
               top: -40,
               left: 0,
               right: 0,
               child: Image.asset(
-                'assets/images/bg.jpg', // Replace with your image path
+                'assets/images/bg.jpg',
                 fit: BoxFit.cover,
-                width: MediaQuery.of(context).size.width, // Ensure full width
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+
+            // Dark Overlay
+            Positioned(
+              top: -40,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 250, // Adjust this height based on your needs
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black.withOpacity(0.6), // Darker overlay
+                      Colors.transparent, // Fades into transparency
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+              ),
+            ),
+
+            // Text and Icon
+            Positioned(
+              top: 59,
+              child: SizedBox(
+                width: screenWidth,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Stroll Bonfire',
+                          style: TextStyle(
+                            fontFamily: 'Proxima',
+                            fontSize: 34,
+                            fontWeight: FontWeight.w700,
+                            color: const Color(0xffafacdd),
+                            shadows: [
+                              Shadow(
+                                offset: const Offset(0, 0),
+                                blurRadius: 7.9,
+                                color: Colors.black.withOpacity(0.2),
+                              ),
+                              const Shadow(
+                                offset: Offset(0, 0),
+                                blurRadius: 2,
+                                color: Color(0xFFBEBEBE),
+                              ),
+                              Shadow(
+                                offset: const Offset(0, 1),
+                                blurRadius: 2,
+                                color: const Color(0xFF24232F).withOpacity(0.5),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5.0),
+                          child: Icon(
+                            Icons.keyboard_arrow_down_outlined,
+                            color: Color(0xffCBC9FF),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
 
@@ -39,24 +112,91 @@ class Screen1 extends StatelessWidget {
                     end: Alignment.center,
                     stops: const [
                       0.2,
-                      1.0
-                    ], // First stop at 10% for transparency
+                      1.0,
+                    ],
                     colors: [
                       Colors.black.withOpacity(0), // Transparent at the top
                       Colors.black, // Full color at the bottom
                     ],
                   ),
                 ),
-                child: const Center(
-                  child: Text(
-                    'Center Content Here',
-                    style: TextStyle(color: Colors.white, fontSize: 24),
+                child: SizedBox(
+                  height: 100,
+                  width: screenWidth,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 60,
+                        width: 60,
+                        // padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          color: Color(
+                              0xff0D0F11), // Background color of the circle
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            'assets/images/image.png',
+                            width: 50,
+                            height: 50,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 50.0, left: 5),
+                            child: Text(
+                              'Angelina, 28', // Replace with your desired text
+                              style: TextStyle(
+                                fontFamily:
+                                    'Proxima', // Ensure the font is added to pubspec.yaml
+                                fontSize: 11,
+                                fontWeight: FontWeight.w700,
+                                color: Color(0xFFF5F5F5),
+                                decoration:
+                                    TextDecoration.none, // No text decoration
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 15.0, top: 9),
+                            child: SizedBox(
+                              width: 247,
+                              height: 60,
+                              child: Text(
+                                "What is your favorite time of the day?",
+                                style: TextStyle(
+                                  fontFamily:
+                                      'Proxima Nova', // Ensure the font is added to pubspec.yaml
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+
+                                  color: Colors.white,
+                                  decoration:
+                                      TextDecoration.none, // No text decoration
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
+      ),
+      bottomNavigationBar: const Padding(
+        padding: EdgeInsets.only(bottom: 15.0),
+        child: Bottomnav(),
       ),
     );
   }
